18018ff8a1dfd8b4881a241f85b6fb25daab9ec5
Matteo Assenza <assenza.matteo@gmail.com>
2019-01-15 11:41:18 -0500

    Initial commit

---
+++ Scripts/core/game.js
@@ -0,0 +1,18 @@
+(function(){
+
+    function Init(params) {
+        
+    }
+    function Start(params) {
+        
+    }
+    function Update(params) {
+        
+    }
+    function Main(params) {
+        
+    }
+
+    window.onload = Init;
+
+})();
